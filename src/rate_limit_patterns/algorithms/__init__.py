f1ac226260e8bab07826af69c10f9e7c831a5375
Aluizio Lira <alumlira@gmail.com>
2026-01-19 22:48:01 -0300

    chore: export RateLimitAlgorithm

--- src/rate_limit_patterns/algorithms/__init__.py
+++ src/rate_limit_patterns/algorithms/__init__.py
@@ -1,1 +1,5 @@
-# Rate limiting algorithm implementations.
+"""Rate limiting algorithm implementations."""
+
+from rate_limit_patterns.algorithms.base import RateLimitAlgorithm
+
+__all__ = ["RateLimitAlgorithm"]
