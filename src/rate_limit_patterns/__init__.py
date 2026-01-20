8f77ec95acaf42307516cc81085b2cc36bedbbe8
Aluizio Lira <alumlira@gmail.com>
2026-01-19 22:49:23 -0300

    chore: export core models

---
+++ src/rate_limit_patterns/__init__.py
@@ -0,0 +1,7 @@
+"""Rate limit patterns library."""
+
+from rate_limit_patterns.models import RateLimitConfig, RateLimitResult
+
+__version__ = "0.1.0"
+
+__all__ = ["RateLimitConfig", "RateLimitResult", "__version__"]
